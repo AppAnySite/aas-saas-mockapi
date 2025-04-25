99f11d2fc5729b216d0edde1da83d6379aece697
Vetagiri Hrushikesh <58148267+Vetagiri-Hrushikesh@users.noreply.github.com>
2025-04-25 10:16:25 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const JWT_API = {
+  secret: 'SECRET-KEY',
+  timeout: '1 days'
+};
+
+export const NO_AUTHENTICATION_REQUIRED = true;
---
+++ src/pages/api/account/register.js
@@ -0,0 +1,33 @@
+import cors from 'utils/cors';
+import users from 'data/users.json';
+import { messages } from 'utils';
+import { NO_AUTHENTICATION_REQUIRED } from 'config';
+
+export default async function handler(req, res) {
+  await cors(req, res, NO_AUTHENTICATION_REQUIRED);
+  const { id, email, password, firstName, lastName } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: messages.errorMessages.enterEmailAndPassword });
+  }
+
+  if (!firstName || !lastName) {
+    return res.status(400).json({ message: messages.errorMessages.enterName });
+  }
+
+  const isAlreadyRegistered = users.find((_user) => _user.email === email);
+
+  if (isAlreadyRegistered) {
+    return res.status(400).json({ message: messages.errorMessages.alreadyRegistered });
+  }
+  const user = {
+    id,
+    email,
+    password,
+    name: `${firstName} ${lastName}`,
+    role: 'User'
+  };
+
+  users.push(user);
+
+  return res.status(200).json(users);
+}
